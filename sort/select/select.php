5755c56a29e989f6417170738a1878df464731a8
Alan Fu <ssisoo@live.cn>
2019-06-11 20:30:25 +0800

    feat(sort/select): 修复选择排序代码

--- sort/select/select.php
+++ sort/select/select.php
@@ -9,12 +9,17 @@ echo json_encode($data) . PHP_EOL;
 
 $length = count($data);
 for($i = 0;$i < $length;$i++){
+    $tmpIndex = $i;
     for($j = $i+1;$j < $length; $j++){
         if($data[$i] > $data[$j]){
-            $tmp = $data[$i];
-            $data[$i] = $data[$j];
-            $data[$j]  = $tmp;
+            $tmpIndex = $j;
         }
+    }
+
+    if($tmpIndex != $i){
+        $tmp = $data[$tmpIndex];
+        $data[$tmpIndex] = $data[$i];
+        $data[$i]  = $tmp;
     }
 }
 echo json_encode($data) . PHP_EOL;
