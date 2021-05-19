53ea14a5c359dd1b63c142a26253bc9fae144e80
Alan <ssiss@live.cn>
2021-05-19 21:04:18 +0800

    dp: 新增注释

--- src/dp/match_str.c
+++ src/dp/match_str.c
@@ -1,3 +1,12 @@
+/**
+ * https://leetcode-cn.com/problems/regular-expression-matching/
+ *
+ * 给你一个字符串s和一个字符规律 p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+ *
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖整个字符串s的，而不是部分字符串。
+ */
 #include "stdio.h"
 #include <string.h>
 #include "stdbool.h"
